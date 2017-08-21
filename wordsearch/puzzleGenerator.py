b5561ac9a30fdf6f8ed9424ef94dd0e65997d983
Alex McCloy <amccloy@gmail.com>
2017-08-22 00:51:18 +1000

    read files in a folder and generate new file if there are none.

--- wordsearch/puzzleGenerator.py
+++ wordsearch/puzzleGenerator.py
@@ -6,8 +6,23 @@
 #       current.txt. Moves the current puzzle into the used folder. If there are no puzzles left will generate a new one
 
 import argparse
+import glob
 
 def rotate_puzzle():
+    for file in glob.glob("new/*.txt"):
+        if debug:
+            print(file)
+
+def generatePuzzle(letterCount):
+    #choose a word with letterCount letters from list
+
+    #check every word in list with <=letterCount letters and see if that word can be made with letterGroup
+
+    #generate a score against each word based on lettercount and word frequency
+
+    #put 10? of the words into a crossword (if there are less than 10 reroll?)
+
+    #format and write to text file
 
 
 
@@ -16,4 +31,11 @@ if __name__ == '__main__':
     parser.add_argument("letterCount", type=int, help="How many letters each puzzle should have")
     parser.add_argument("puzzleCount", type=int, help="How many puzzles to generate")
     parser.add_argument("--getPuzzle", "-g", action="store_true", help="rotates a new puzzle")
-    
+    parser.add_argument("--debug", "-d", action="store_true", help="extra debugging text")
+    args = parser.parse_args()
+
+    if args.getPuzzle:
+        rotate_puzzle()
+        break
+
+    for (i in range(0,puzzleCount)):
