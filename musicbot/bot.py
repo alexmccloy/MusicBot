2f09f826cbde2fd508f6e23906fb0ea8bdef8580
Alex McCloy <amccloy@gmail.com>
2017-09-09 17:23:48 +1000

    debug messages

--- musicbot/bot.py
+++ musicbot/bot.py
@@ -2101,6 +2101,7 @@ class MusicBot(discord.Client):
         while not self.finished:
             #allow dequeueThread to run the game, while printing the crossword status every 10 secs
             if counter % 10 == 0:
+                print(counter)
                 await self.safe_send_message(channel, formatCrosswordOutput(gameManager.crossword, gameManager.letters))
             result = dequeueThread.join(False)
             await asyncio.sleep(1)
